099fcc437600242011b942e710f11a8ebfee5d21
faph <faph@users.noreply.github.com>
2014-11-14 19:13:54 +0000

    error corrected

--- lmoments3/__init__.py
+++ lmoments3/__init__.py
@@ -194,7 +194,8 @@ def _samlmusmall(x, nmom=5):
     sum_xtrans = sum([(comb1[i] - comb1[n - i - 1]) * x[i] for i in range(n)])
     l2 = coefl2 * sum_xtrans
 
-    if nmom == 2:    return [l1, l2]
+    if nmom == 2:
+        return [l1, l2]
 
     # Third L-moment
 
